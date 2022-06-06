86cf414d85d8fb8decc17e75c38606ef9e3147fc
Tyler Emmerson <TylerEmmerson0216@gmail.com>
2022-06-06 15:52:27 -0700

    add a list of grocery item inputs, arrange them in alphabetical order and submit as a li. Co-authored-by: Sandra Tena <sandratena06@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("form#grocery").submit(function(event) {
+    event.preventDefault();
+    console.log("this is working");
+    const grocery1 = ($("#grocery1").val());
+    const grocery2 = ($("#grocery2").val());
+    const grocery3 = ($("#grocery3").val());
+    let Array = [grocery1, grocery2, grocery3];
+    Array.sort();
+      
+    $("#item1").text(Array[0]);
+    $("#item2").text(Array[1]);
+    $("#item3").text(Array[2]);
+
+    $("#result-show").show();
+    });
+  });
+$(".clickable").click(function() {
+  $("#result-show").toggle();
+});
